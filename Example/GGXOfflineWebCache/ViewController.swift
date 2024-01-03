87d555324269cda16a9da9ac202537aa4b26660d
小修 <gaoguangxiao125@sina.com>
2024-01-03 16:22:16 +0800

    Initial commit

---
+++ Example/GGXOfflineWebCache/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GGXOfflineWebCache
+//
+//  Created by 小修 on 01/03/2024.
+//  Copyright (c) 2024 小修. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
